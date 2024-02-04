7724eaf15692599d0add1a735d1341b5efb778c1
Saleem Ullah <saleemullah8818@gmail.com>
2024-02-04 07:22:37 +0500

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path"); // Added for path module
 require("dotenv").config();
 
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
